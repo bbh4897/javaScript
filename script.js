52e4c9d8b1950315484be5242bd17fb5738bad94
bbg <bagbrc@gmail.com>
2018-09-01 17:37:43 +0300

    basit işlemler

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+// ekrana yazı yazdırma
 document.write("Hello World!");
 
+// Değişken tanımlama ve ekrana yazdırma
 var i = 2;
-
 document.write(i);
+
+//kutuda uyarı mesajı yazdırma
+
+alert("Bu bir uyarı mesajıdır.");
+
+// Fonksiyon yazma
+
+function deneme(){
+    alert("Deneme Fonksıyonu");
+    
+    
+    
+}
+
+
