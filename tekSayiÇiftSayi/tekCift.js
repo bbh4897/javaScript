da1b075e7bb5f4e17a707a1b4efaab61bce8e540
bbg <bagbrc@gmail.com>
2018-09-06 20:40:47 +0300

    Alıştırmalar

---
+++ tekSayiÇiftSayi/tekCift.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+   var sayi = prompt("Sayı Giriniz: ");
+
+    if(sayi%2==0){
+        document.write("Sayı Çift");
+    }
+    else{
+        document.write("Sayı tek");
+    }
+});
